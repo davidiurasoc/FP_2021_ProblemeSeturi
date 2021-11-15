e8996c32873a0cb6f9a99a32232375f6784b96c1
Iurasoc David <iurasoc30@gmail.com>
2021-11-15 18:50:33 +0100

    Solved problems: 2

--- Probleme Seturi/Set 1/Program.cs
+++ Probleme Seturi/Set 1/Program.cs
@@ -10,16 +10,16 @@ namespace Set_1
     {
         static void Main(string[] args)
         {
-            // problema1();  rezolvata
-            // problema2();
-            // problema3();  //rezolvata
-            // problema4();  //rezolvata
+            // problema1();  // rezolvata
+            // problema2();  // rezolvata
+            // problema3();  // rezolvata
+            // problema4();  // rezolvata
             // problema5();
             // problema6();
-            // problema7();  //rezolvata
-            // problema8();  //rezolvata
-            // problema9();  //rezolvata
-            // problema10(); //rezolvata
+            // problema7();  // rezolvata
+            // problema8();  // rezolvata
+            // problema9();  // rezolvata
+            // problema10(); // rezolvata
             // problema11();
             // problema12();
             // problema13();
@@ -244,6 +244,57 @@ namespace Set_1
 
         private static void problema2()
         {
+            Console.WriteLine();
+            Console.WriteLine("Problema 2: Rezolvati ecuatia de gradul 2 cu o necunoscuta: ax^2 + bx + c = 0, unde a, b si c sunt date de intrare. ");
+            Console.WriteLine("Tratati toate cazurile posibile. ");
+            Console.WriteLine();
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int dt;
+            double x1, x2;
+
+            Console.WriteLine();
+
+            if (a == 0)
+                if (b == 0) {
+                    if (c == 0)
+                    {
+                        Console.WriteLine("Ecuatie nedeterminata!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ecuatie imposibila!");
+                    }
+                }
+                else
+                {
+                    x1 = -c / b;
+                    Console.WriteLine("Ecuatie de gradul 1 cu x1 = {0}", x1);
+                }
+            else
+            {
+                dt = b * b - 4 * a * c;
+                if (dt < 0)
+                {
+                    Console.WriteLine("Ecuatia are solutii complexe!");
+                }
+                else
+                {
+                    if (dt == 0)
+                    {
+                        x1 = x2 = -b / (2 * a);
+                        Console.WriteLine("x1 = x2 = {0}", x1);
+                    }
+                    else
+                    {
+                        x1 = (-b + Math.Sqrt(dt)) / (2 * a);
+                        x2 = (-b - Math.Sqrt(dt)) / (2 * a);
+                        Console.WriteLine("Solutiile sunt x1 = {0}, x2 = {1}", x1, x2);
+                    }
+                }
+            }
         }
 
         private static void problema1()
