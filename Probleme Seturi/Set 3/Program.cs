ab6285bf32f0421661e92040d1380e102ea77499
Iurasoc David <iurasoc30@gmail.com>
2022-01-07 15:10:41 +0100

    Solved problems: 1

--- Probleme Seturi/Set 3/Program.cs
+++ Probleme Seturi/Set 3/Program.cs
@@ -10,6 +10,20 @@ namespace Set_3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Calculati suma elementelor unui vector de n numere citite de la tastatura. Rezultatul se va afisa pe ecran.");
+            Console.WriteLine();
+
+
+            Console.Write("Introduceti un numar: "); int n = int.Parse(Console.ReadLine());
+            int[] v = new int[n];
+            int sum = 0;
+
+            for (int i = 0; i < v.Length; i++)
+            {
+                Console.Write("Introduceti un numar: "); v[i] = int.Parse(Console.ReadLine());
+                sum += v[i];
+            }
+            Console.WriteLine($"Suma celor {n} numere este: {sum}");
         }
     }
 }
